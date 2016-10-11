74367dec965f941f0c9f570946a6c11e2efbc29b
jwolf059 <jwolf059@uw.edu>
2016-10-10 18:06:47 -0700

    Next step

--- app/src/main/java/tcss450/uw/edu/thiswillwork/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/thiswillwork/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //I'm testing someting here - Thang
+        // I changed this - Jeremy
     }
 }
--- app/src/main/java/tcss450/uw/edu/thiswillwork/ThangActivity.java
+++ app/src/main/java/tcss450/uw/edu/thiswillwork/ThangActivity.java
@@ -9,5 +9,6 @@ public class ThangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thang);
+        //YEP changed this
     }
 }
