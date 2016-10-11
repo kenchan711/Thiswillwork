89b48adb94ed8da265bb296ac9ec1825e62f67ec
thangsea <thangvn@uw.edu>
2016-10-10 17:24:00 -0700

    Thang's new activity

---
+++ app/src/main/java/tcss450/uw/edu/thiswillwork/ThangActivity.java
@@ -0,0 +1,13 @@
+package tcss450.uw.edu.thiswillwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thang);
+    }
+}
